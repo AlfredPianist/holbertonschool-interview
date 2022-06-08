307dbc5bb9871489b487063d965626a6dbe896b1
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-08 12:23:37 -0500

    Add project 0x07

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle- Checks if a singly linked list has a cycle in it.
+ * @list: The list's head.
+ *
+ * Return: 1 if singly linked list has a cycle. 0 otherwise.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *uro, *boros;
+
+	if (list == NULL)
+		return (0);
+
+	uro = list;
+	boros = list;
+
+	while (1)
+	{
+		if (uro == NULL || uro->next == NULL)
+			return (0);
+		uro = uro->next->next;
+		boros = boros->next;
+		if (uro == boros)
+			break;
+	}
+
+	return (1);
+}
