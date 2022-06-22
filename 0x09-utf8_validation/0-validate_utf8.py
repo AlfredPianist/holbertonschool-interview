826260090c3ca4829f4f05a9a79244ce88925462
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-22 17:14:37 -0500

    Fix: 0x09

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -12,7 +12,20 @@ def validUTF8(data):
         Returns:
             bool: True if data is a valid UTF-8 encoding, False otherwise.
     """
-    for byte in data:
-        if not 0 <= byte <= 255:
+    char_num_bytes = 0
+    mask_msb_1 = 1 << 7
+    mask_msb_2 = 1 << 6
+    for num in data:
+        mask = 1 << 7
+        if char_num_bytes == 0:
+            while mask & num:
+                char_num_bytes += 1
+                mask >>= 1
+            if char_num_bytes == 0:
+                continue
+            if char_num_bytes == 1 or char_num_bytes > 4:
+                return False
+        elif not (num & mask_msb_1 and not (num & mask_msb_2)):
             return False
-    return True
+        char_num_bytes -= 1
+    return char_num_bytes == 0
