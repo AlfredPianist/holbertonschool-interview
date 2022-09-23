2e8dcdf73e75480e1134724359b1e7b587ab74e0
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-09-22 23:16:15 -0500

    Add: project 0x15

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void _puts(char *str);
+int num_of_digits(char *str);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void multiply(char *p_prod, char *n1, char *n2, int *len_n1, int *len_n2);
+
+#endif /* _HOLBERTON_H_ */
