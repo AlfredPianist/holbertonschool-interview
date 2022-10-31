c86310b418d42ef6864faa0ea6cd85969a3aac44
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-10-31 11:42:10 -0500

    DP efficient solution for 0x19

--- 0x19-making_change/0-main.py
+++ 0x19-making_change/0-main.py
@@ -2,9 +2,50 @@
 """
 Main file for testing
 """
-
+import time
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 2, 25], 37))
+# Tests in checker order
 
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+# Small total
+assert makeChange([1, 2, 10], 11) == 2
+assert makeChange([3, 7, 11, 4, 16, 21], 2) == -1
+
+# Large total
+assert makeChange([1, 4, 5, 10], 1278652) == 127867
+assert makeChange([3, 6, 9], 1278652) == -1
+
+# Not allowed total
+assert makeChange([1, 3, 7], -10) == 0
+assert makeChange([10, 25, 50], 0) == 0
+
+# Normal setting
+assert makeChange([1, 2, 5, 10, 25, 50, 100], 972) == 13
+
+# No coins
+assert makeChange([], 1) == -1
+
+# Long coin list
+coins = []
+for i in range(1, 1000, 7):
+    coins.append(i)
+assert makeChange(coins, 16576) == 21
+
+# Non greedy solution
+assert makeChange([507, 500, 300, 200, 6, 5, 4, 3], 1413) == 5
+
+# Runtime check, possible total
+start = time.time()
+for i in range(10):
+    makeChange([1, 4, 5, 10], 1278652)
+end = time.time()
+avg = (end - start) / 10
+assert avg <= 3
+
+# Runtime check, impossible total
+start = time.time()
+for i in range(10):
+    makeChange([2, 4, 6, 10], 1278653)
+end = time.time()
+avg = (end - start) / 10
+assert avg <= 3
--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,6 +5,7 @@ Given a pile of coins of different values,
 determine the fewest number of coins needed to
 meet a given amount total
 """
+import sys
 
 
 def makeChange(coins, total):
@@ -22,15 +23,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
+    inf = sys.maxsize
+    combinations_array = [inf for idx in range(total + 1)]
+    combinations_array[0] = 0
 
-    amount = 0
-    for coin in coins:
-        if total == 0:
-            return amount
-        amount += total // coin
-        total %= coin
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                combinations_array[i] = min(combinations_array[i],
+                                            combinations_array[i - coin] + 1)
 
-    if total == 0:
-        return amount
-    return -1
+    if combinations_array[total] == inf:
+        return -1
+    return combinations_array[total]
