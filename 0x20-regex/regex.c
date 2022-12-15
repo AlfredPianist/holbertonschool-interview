a1bac3a403ea4b1311013ac477954a552aa1c88b
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-12-15 16:05:27 -0500

    Add: project 0x20

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,33 @@
+#include "regex.h"
+
+/**
+ * regex_match - checks whether a given pattern matches a given string.
+ * @str: the source string.
+ * @pattern: the pattern to be matched.
+ *
+ * Return: 1 if pattern matches, 0 otherwise.
+ */
+int regex_match(char const *str, char const *pattern)
+{
+	int dot = 0, star = 0;
+
+	if (!(str && pattern))
+		return (0);
+	
+	dot = *str && ((*pattern == '.') || (*str == *pattern));
+	star = *(pattern + 1) == '*';
+
+	if (!(*str || star))
+		return (*pattern ? 0 : 1);
+
+	if (star)
+	{
+		if (dot)
+			return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+		return (regex_match(str, pattern + 2));
+	}
+	else if (dot)
+		return (regex_match(str + 1, pattern + 1));
+	return (0);
+}
+
---
+++ 0x20-regex/regex.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+int regex_match(char const *str, char const *pattern);
+
+#endif
